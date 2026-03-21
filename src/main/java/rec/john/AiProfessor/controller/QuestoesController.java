e4f8c18afea9322cb06e0e855de8ed92bcf79087
João Manuel <johnjohn200513@gmail.com>
2026-03-21 12:45:44 -0300

    ♻️refactor(QuestoesController): altera metodo HTTP da funcao de gerar questoes

--- src/main/java/rec/john/AiProfessor/controller/QuestoesController.java
+++ src/main/java/rec/john/AiProfessor/controller/QuestoesController.java
@@ -3,12 +3,9 @@ package rec.john.AiProfessor.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import rec.john.AiProfessor.domain.ia.GeminiService;
-import rec.john.AiProfessor.domain.questoes.DadosListaQuestoesPROMPT;
+import rec.john.AiProfessor.domain.questoes.DadosListaQuestoes;
 import rec.john.AiProfessor.domain.questoes.ListaQuestoes;
 
 import javax.validation.Valid;
@@ -22,8 +19,8 @@ public class QuestoesController  {
 
 
 
-    @GetMapping
-    public ResponseEntity<ListaQuestoes> gerarQuestoes(@RequestBody @Valid DadosListaQuestoesPROMPT prompt) {
+    @PostMapping
+    public ResponseEntity<ListaQuestoes> gerarQuestoes(@RequestBody @Valid DadosListaQuestoes prompt) {
         ListaQuestoes listaQuestoes = geminiService.gerarListaQuestoes(prompt);
 
         return ResponseEntity.ok(listaQuestoes);
