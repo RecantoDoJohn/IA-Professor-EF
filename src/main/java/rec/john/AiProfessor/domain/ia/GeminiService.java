1ec85f07e12a1bf226295dbe0ebc7b4c25f0d681
João Manuel <johnjohn200513@gmail.com>
2026-03-31 16:31:50 -0300

    ✨feat(CORS): adciona novos campos aos prompts

--- src/main/java/rec/john/AiProfessor/domain/ia/GeminiService.java
+++ src/main/java/rec/john/AiProfessor/domain/ia/GeminiService.java
@@ -1,5 +1,6 @@
 package rec.john.AiProfessor.domain.ia;
 
+import jakarta.validation.constraints.Null;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.google.genai.GoogleGenAiChatOptions;
 import org.springframework.core.ParameterizedTypeReference;
@@ -18,8 +19,16 @@ public class GeminiService {
     }
 
     public ListaQuestoes gerarListaQuestoes(DadosListaQuestoes promptRaw) {
-        String prompt = String.format("Como um professor do ensino fundamental, gere %d questões de múltipla escolha e %d questoes objetivas sobre a habilidade BNCC \"%s\" para alunos do %s do ensino fundamental.",
+        String prompt = String.format("Como um professor especialista do ensino fundamental, gere %d questões de múltipla escolha com as letras e %d questoes subjetivas com espacos de quebra de linha para a resposta ambos sobre a habilidade BNCC \"%s\" para alunos do %s do ensino fundamental. pode usar quebras de linha e pode fazer questoes com subitens e com o enunciado",
                 promptRaw.quantQuestObj(), promptRaw.quantQuestSub(), promptRaw.habilidadeBNCC(), promptRaw.anoFundamental());
+
+        if (promptRaw.dificuldade() != null) {
+            prompt += String.format("as questoes devem ser de dificultade %s", promptRaw.dificuldade());
+        }
+
+        if (promptRaw.tamanho() != null) {
+            prompt += String.format("as questoes devem ser de tamanho %s", promptRaw.tamanho());
+        }
 
         return this.chatClient.prompt().user(prompt)
                 .options(
@@ -30,8 +39,24 @@ public class GeminiService {
     }
 
     public PlanoDeAula gerarPlanoDeAula(DadosPlanoDeAulo promptRaw) {
-        String prompt = String.format("Como um professor do ensino fundamental, gere um plano de aula com a(s) habilidade(s) %s seguindo a BNCC, para o %s ano do ensino fundamental da area de %s",
+        String prompt = String.format("Como um professor do ensino fundamental, gere um plano de aula com a habilidade %s seguindo a BNCC, para o %s ano do ensino fundamental da area de %s, pode usar quebras de linha",
                  promptRaw.habilidadeBNCC(), promptRaw.anoFundamental(), promptRaw.areaConhecimento());
+
+        if (promptRaw.duracao() != null) {
+            prompt += String.format("devem ser de duracao %s", promptRaw.duracao());
+        }
+
+        if (promptRaw.recursosDidaticos() != null) {
+            prompt += String.format("devem usar %s como recursos didaticos", promptRaw.recursosDidaticos());
+        }
+
+        if (promptRaw.duracao() != null) {
+            prompt += String.format("devem ser de duracao %s", promptRaw.duracao());
+        }
+
+        if (promptRaw.avaliacao() != null) {
+            prompt += String.format("devem ter como avaliacao %s", promptRaw.avaliacao());
+        }
 
         return this.chatClient.prompt().user(prompt)
                 .options(
